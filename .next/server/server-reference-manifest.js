49e8c2a828a17ac9b777992514df634b12117ed4
Godwin Bamisaye <godwinbamisaye3@gmail.com>
2025-12-23 04:02:07 +0100

    UI BUG FIX

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"1p6kvmoXEE3qSsoKNqafs/Q1qUZ2ym71mc4jm3CGKmA=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"}"
--- .next/static/chunks/src_app_aboutme_page_tsx_f8fdd2d5._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_5bed1ac5._.js",
-  "static/chunks/src_f9add5d1._.js"
-],
-    source: "dynamic"
-});
